6565529a60c969d0ab8335811613e42d6ef5d7cd
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-09-28 10:23:08 +0100

    Service layer, component registration

--- src/main/java/com/webservicesproject/project/resources/UserResource.java
+++ src/main/java/com/webservicesproject/project/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.webservicesproject.project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webservicesproject.project.entities.User;
+import com.webservicesproject.project.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired //in order to the automatic injection using 'Autowired' works, we have to set the 'UserService' as a 'Component' of Spring
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmil.com", "99999999", "12345");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
 		
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value ="/{id}") //'PathVariable' in order to Spring to consider it as a URL parameter
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 }
