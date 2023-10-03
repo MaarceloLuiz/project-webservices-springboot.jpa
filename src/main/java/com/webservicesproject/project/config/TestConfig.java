2f5ca753ecc511e401abd06932ca32ac45f0785a
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-10-03 07:35:54 +0100

    OrderItem, ManyToMany association with extra attributes - part 1

--- src/main/java/com/webservicesproject/project/config/TestConfig.java
+++ src/main/java/com/webservicesproject/project/config/TestConfig.java
@@ -49,6 +49,7 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		//associating the products with the categories
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
